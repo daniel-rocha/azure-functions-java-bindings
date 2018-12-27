83df770d023d5cb883a35fe36f17ab998ee88ded
Daniel Rocha <dcrocha@gmail.com>
2018-12-27 16:11:15 +0800

    Added link to github issue with BlobTrigger types

--- src/main/java/com/danielrocks/function/blob/trigger/TriggerPojo.java
+++ src/main/java/com/danielrocks/function/blob/trigger/TriggerPojo.java
@@ -11,7 +11,9 @@ import com.microsoft.azure.functions.annotation.FunctionName;
  * 
  * The following example shows a Java function that uses the BlobTrigger
  * annotation to be notified of changes to a certain storage container.
- *
+ * 
+ * https://github.com/Azure/azure-functions-java-worker/issues/256 
+ * 
  */
 
 public class TriggerPojo {
--- src/main/java/com/danielrocks/function/blob/trigger/TriggerString.java
+++ src/main/java/com/danielrocks/function/blob/trigger/TriggerString.java
@@ -13,6 +13,7 @@ import com.microsoft.azure.functions.annotation.BlobTrigger;
  * The following example shows a Java function that uses the BlobTrigger
  * annotation to be notified of changes to a certain storage container.
  *
+ * https://github.com/Azure/azure-functions-java-worker/issues/256
  */
 
 public class TriggerString {
