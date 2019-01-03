51c63a7448ff05373324df3b483e57c682e3ccfa
Hannes Nel <Hannes.Nel@windowslive.com>
2019-01-03 19:18:42 +1300

    Updated script to create a queue and put a message in it.

--- env/setupenvironment.ps1
+++ env/setupenvironment.ps1
@@ -24,6 +24,17 @@ $storageContainerName = "samples-workitems"
 New-AzStorageContainer -Name $storageContainerName -Permission Container -Context $storageAccount.Context
 Set-AzStorageBlobContent -File "./testdata.txt" -Blob "testdata.txt" -Container $storageContainerName -Context $storageAccount.Context -Force
 
+$storageQueueName = "myqueue-items-sample"
+$storageQueue = New-AzStorageQueue -Name $storageQueueName -Context $storageAccount.Context
+
+# Create a new message using a constructor of the CloudQueueMessage class
+$queueMessage = New-Object -TypeName Microsoft.WindowsAzure.Storage.Queue.CloudQueueMessage `
+  -ArgumentList "testdata.txt"
+
+  # Add a new message to the queue
+$storageQueue.CloudQueue.AddMessageAsync($QueueMessage)
+
+
 $settingsFileTemplate = Get-Content template.local.settings.json
 $settingsFileTemplate -replace "{storageConnectionString}", $storageConnectionString | Set-Content ../local.settings.json
 
